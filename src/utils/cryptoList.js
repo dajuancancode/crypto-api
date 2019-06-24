744cdb8ce486ba307306ff229f81e3c3ec819585
DaJuan Harris <dajuancancode@gmail.com>
2019-06-23 22:08:42 -0400

    Add ID to cryptoList

--- src/utils/cryptoList.js
+++ src/utils/cryptoList.js
@@ -9,7 +9,8 @@ const headers = {'X-CMC_PRO_API_KEY': CRYPTO_API_KEY}
 
 // Need to send name, symbol, price, percent_change_24h, percent_change_7d
 class CryptoObject {
-  constructor(name, symbol, price, percent_change_24h, percent_change_7d) {
+  constructor(id, name, symbol, price, percent_change_24h, percent_change_7d) {
+    this.id                 = id;
     this.name               = name ;
     this.symbol             = symbol;
     this.price              = price;
@@ -22,12 +23,14 @@ const cryptoList = async (callback) => {
   try {
     const {data: {data}} = await axios.get(url, {headers})
     let cryptoArray = []
+    let i = 1
     data.forEach(crypto => {
       let newCrypto = new CryptoObject(
-        crypto.name, crypto.symbol, crypto.quote.USD.price,
+        i, crypto.name, crypto.symbol, crypto.quote.USD.price,
         crypto.quote.USD.percent_change_24h, crypto.quote.USD.percent_change_7d
       )
       cryptoArray.push(newCrypto)
+      i++
     })
     callback(undefined, cryptoArray)
   } catch (error) {
